bb6123ca45ff0256a874daf49be7b05e6ec963bc
Junseokee <weq156@naver.com>
2022-08-12 08:57:15 +0900

    포스트 목록 페이지에 부트스트랩 적용하기

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
-def single_post_page(request, pk):
-    post = Post.objdects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post' : post,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
--- single_pages/urls.py
+++ single_pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.about_me),
+    path('about_me/', views.about_me),
     path('', views.landing),
 
 ]

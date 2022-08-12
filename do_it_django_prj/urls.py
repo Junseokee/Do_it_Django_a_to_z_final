9ebf8597262b91b811d322bf7d1d874ab80e54df
Junseokee <weq156@naver.com>
2022-08-12 09:33:22 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -1,15 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls'))
 ]
-# {% for p in post_list %}
-#     </hr>
-#     <h2><a href="{{ p.get_absolute_url }}">{{ p.title }}</a></h2>
-#     <h4>{{ p.created_at }}</h4>
-#     <p>{{ p.content }}</p>
-# {% endfor %}
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
